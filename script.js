3fc4f1adf7dc6f8f4c4cb91d4f5be510b61e6a88
Daniel Fogerty <Danjfogerty@gmail.com>
2017-02-01 11:06:12 -0800

    add player token started

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 /**
  * Created by danielfogerty on 2/1/17.
  */
+var current_turn = 1;
+var position_array = [2, 0, 0, 0, 0, 0, 0];
+var game_array = null;
+
+$(document).ready(initialize_game);
+
+function initialize_game() {
+    game_array = $('.column');
+    $('.column').click(add_player_token);
+}
+
+function add_player_token() {
+    for(var i = 0; i < game_array.length; i++) {
+        if (this == game_array[i]) {
+            var current = $(game_array[i]);
+            console.log(current);
+        }
+    }
+}
