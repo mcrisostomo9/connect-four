64936ce013013d8e5e4429f2c15feaf04af0a9d7
Daniel Fogerty <Danjfogerty@gmail.com>
2017-02-01 11:24:10 -0800

    add player tokens working as intended

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 /**
  * Created by danielfogerty on 2/1/17.
  */
-var current_turn = 1;
-var position_array = [2, 0, 0, 0, 0, 0, 0];
+var current_turn = 0;
+var position_array = [1, 1, 1, 1, 1, 1, 1];
 var game_array = null;
+var game_state = [[],[],[],[],[],[],[]];
 
 $(document).ready(initialize_game);
 
@@ -13,10 +14,31 @@ function initialize_game() {
 }
 
 function add_player_token() {
-    for(var i = 0; i < game_array.length; i++) {
+    for (var i = 0; i < game_array.length; i++) {
         if (this == game_array[i]) {
-            var current = $(game_array[i]);
-            console.log(current);
+            if (position_array[i] > 6) {
+                return;
+            }
+            if(current_turn === 1) {
+                $(this).find('.cell:nth-child(' + position_array[i] + ')').find('.player-token').addClass('p1-token');
+                game_state[i][position_array[i]] = 1;
+                position_array[i]++;
+                current_turn = 0;
+            } else {
+                $(this).find('.cell:nth-child(' + position_array[i] + ')').find('.player-token').addClass('p2-token');
+                game_state[i][position_array[i]] = 0;
+                position_array[i]++;
+                current_turn = 1;
+            }
         }
     }
+    check_win();
+}
+
+function check_win() {
+
+}
+
+function reset_game() {
+    
 }
